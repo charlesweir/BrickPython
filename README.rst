a1911ced53473d7ae6e5f199e42dc015493999f1
Charles Weir <charles@penrillian.com>
2014-02-23 23:31:24 +0000

    CW: Started working on packaging.

---
+++ README.rst
@@ -0,0 +1,4 @@
+BrickPython
+===========
+
+Extensions to BrickPi_Python to support always-responsive programs and using NXT motors as servos 
--- setup.py
+++ setup.py
@@ -3,14 +3,85 @@
 # These files have been made available online through a Creative Commons Attribution-ShareAlike 3.0  license.
 # (http://creativecommons.org/licenses/by-sa/3.0/)
 
-import setuptools
+# import setuptools
+#
+# setuptools.setup(
+# 	name="BrickPython",
+# 	description="Python structure for the BrickPi",
+# 	author="Charles and James Weir",
+# 	url="http://www.charlesweir.com/",
+# 	py_modules=['BrickPython'],
+# 	test_suite='test'
+# 	#install_requires=open('requirements.txt').readlines(),
+# )
 
-setuptools.setup(
-	name="BrickPython",
-	description="Python structure for the BrickPi",
-	author="Charles and James Weir",
-	url="http://www.charlesweir.com/",
-	py_modules=['BrickPython'],
-	test_suite='test'
-	#install_requires=open('requirements.txt').readlines(),
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+import codecs
+import os
+import sys
+import re
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+def read(*parts):
+    # intentionally *not* adding an encoding option to open
+    return codecs.open(os.path.join(here, *parts), 'r').read()
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+long_description = read('README.rst')
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = ['--strict', '--verbose', '--tb=long', 'test/TestSensor.py', 'test/TestMotor.py', 'test/TestScheduler.py', 'test/TestBrickPiWrapper.py']
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+setup(
+    name='BrickPython',
+    version=find_version('BrickPython', '__init__.py'),
+#     url='http://github.com/jeffknupp/sandman/',
+    license='Apache Software License',
+    author='Charles Weir',
+    tests_require=['pytest'],
+    install_requires=[],
+    cmdclass={'test': PyTest},
+    author_email='charles@penrillian.com',
+    description='Python interface for the BrickPi using Objects and Coroutines',
+    long_description=long_description,
+    entry_points={
+        },
+    packages=['BrickPython','test'],
+    include_package_data=True,
+    platforms='any',
+    test_suite='test',
+    zip_safe=False,
+#     package_data={'BrickPython': ['templates/**', 'static/*/*']},
+    classifiers = [
+        'Programming Language :: Python',
+        'Development Status :: 4 - Beta',
+        'Natural Language :: English',
+        'Environment :: Raspberry Pi',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Libraries :: Application Frameworks'
+        ],
+    extras_require={
+        'testing': ['pytest'],
+      },
+	#install_requires=open('requirements.txt').readlines()  # It doesn't need it for Mac.
 )
