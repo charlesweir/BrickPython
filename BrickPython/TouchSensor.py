54450e3dc634f0911a06fe1152bdb7e278b38b04
Charles Weir <charles@penrillian.com>
2014-03-20 23:21:02 +0000

    Added TouchSensor class and tests.

---
+++ BrickPython/TouchSensor.py
@@ -0,0 +1,28 @@
+# TouchSensor - represents an NXT touch sensor supported by the BrickPython library
+#
+# Copyright (c) 2014 Charles Weir.  Shared under the MIT Licence.
+
+from Sensor import Sensor
+
+class TouchSensor(Sensor):
+    '''TouchSensor, representing a sensor attached to one of the BrickPi ports.
+    Parameter *port* may be either a value (BrickPi.PORT_1) or an integer '1'-'5'
+
+    Just using the BrickPi TYPE_SENSOR_TOUCH didn't work for me; hence this.
+    '''
+    def __init__(self, port):
+        Sensor.__init__(self, port, Sensor.RAW)
+        #: Function that gets called with new value as parameter when the value changes - default, none.
+        self.callbackFunction = lambda x: 0
+
+    def updateValue(self, newValue):
+        previousValue = self.recentValue
+        Sensor.updateValue(self, newValue)
+        self.recentValue = True if self.recentValue > 500 else False
+        if self.recentValue != previousValue:
+            self.callbackFunction(self.recentValue)
+
+    def waitForChange(self):
+        previousValue = self.recentValue
+        while self.recentValue == previousValue:
+            yield
