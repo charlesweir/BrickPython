a310d021fd18d4a845e2d5c4a87d2bf5dba5a6e1
Charles Weir <charles@penrillian.com>
2014-03-22 13:39:49 +0000

    Moved all Sensor tests into one file.

--- test/TestSensor.py
+++ test/TestSensor.py
@@ -4,10 +4,11 @@
 
 import unittest
 from BrickPython.BrickPi import PORT_1
-from BrickPython.Sensor import Sensor
+from BrickPython.Sensor import Sensor, TouchSensor
+import TestScheduler
 
 class TestSensor(unittest.TestCase):
-    'Test for the Sensor object'
+    'Tests for the Sensor classes'
 
     def testSensor(self):
         sensor = Sensor( PORT_1 )
@@ -24,6 +25,41 @@ class TestSensor(unittest.TestCase):
         self.assertEquals( repr(Sensor( '1' ) ), 'Sensor 1: 0 (0)')
         self.assertEquals( repr(Sensor( '1', Sensor.COLOR_NONE ) ), 'Sensor 1: 0 (0)')
 
+    def testTouchSensor(self):
+        sensor = TouchSensor( '1' )
+        self.assertEquals(sensor.port, 0)
+        self.assertEquals( sensor.idChar, '1' )
+        self.assertEquals( sensor.value(), True ) # Pressed in
+        sensor.updateValue( 1000 )
+        self.assertEquals( sensor.value(), False )
+
+    def testTouchSensorTextRepresentation(self):
+        self.assertEquals( repr(TouchSensor( '1' ) ), 'TouchSensor 1: True (0)')
+
+    def testCallbackWhenChanged(self):
+        result = [True]
+        def callbackFunc(x):
+            result[0] = x
+        sensor = TouchSensor( '1' )
+        sensor.callbackFunction = callbackFunc
+        sensor.updateValue( 1000 )
+        self.assertEquals( result[0], False )
+        # And no call when it doesn't change
+        result[0] = True
+        sensor.updateValue( 1000 )
+        self.assertEquals( result[0], True )
+        # But does get a call when it changes back
+        result[0] = False
+        sensor.updateValue( 20 )
+        self.assertEquals( result[0], True )
+
+    def testCoroutineWaitingForChange(self):
+        sensor = TouchSensor( '1' )
+        coroutine = sensor.waitForChange()
+        coroutine.next()
+        coroutine.next()
+        sensor.updateValue( 1000 )
+        TestScheduler.TestScheduler.checkCoroutineFinished( coroutine )
 
 if __name__ == '__main__':
     unittest.main()
